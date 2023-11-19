ab64d67f601bc6ae11f2476a499a43b779a467d8
sindhurajareddy <133632587+sindhurajareddy@users.noreply.github.com>
2023-11-19 21:40:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "Terraform-s3" {
-    bucket = vars.bucket
-    force_destroy = vars.force_destroy
+resource "aws_s3_bucket" "example1" {
+    bucket = var.bucket
+    acl = "private"
 }
 
-resource "aws_s3_bucket_versioning" "Terraform-s3-versioning" {
-    bucket = aws_s3_bucket.Terraform-s3.id
-    versioning_configuration {
-      status = vars.versioning
-    }
-}
+
